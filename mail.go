fbf4518d399b3fb6c59b3a44e8c0c796f44cc4cf
Scott Lawrence <bytbox@gmail.com>
2012-01-21 15:30:43 -0500

    Testing stubs

--- mail.go
+++ mail.go
@@ -1,3 +1,19 @@
 // Package mail implements a parser for electronic mail messages as specified
 // in RFC2822.
 package mail
+
+type Message struct {
+
+}
+
+func Parse(s string) (m Message) {
+	return
+}
+
+func getHeaders(s string) (hs []string) {
+	return
+}
+
+func splitHeader(s string) (k, v string) {
+	return
+}
--- mail_test.go
+++ mail_test.go
@@ -4,6 +4,39 @@ import (
 	"testing"
 )
 
-func TestParse(t *testing.T) {
+type getHeadersTest struct {
 
+}
+
+func TestGetHeaders(t *testing.T) {
+
+}
+
+type splitHeadersTest struct {
+
+}
+
+func TestSplitHeader(t *testing.T) {
+
+}
+
+type hdr struct {
+	key, val string
+}
+
+type parseTest struct {
+	orig string
+	hdrs []string
+	cont string
+}
+
+var parseTests = []parseTest{
+
+}
+
+func TestParse(t *testing.T) {
+	for _, pt := range parseTests {
+		m := Parse(pt.orig)
+		m = m
+	}
 }
