13865e92aa707c8257d31b1d3c4526b0dec0ba16
Scott Lawrence <bytbox@gmail.com>
2012-01-21 16:09:23 -0500

    Full splitHeadersTests

--- mail_test.go
+++ mail_test.go
@@ -1,10 +1,20 @@
 package mail
 
 import (
+	"strings"
 	"testing"
 )
 
+// Converts all newlines to CRLFs.
+func crlf(s string) string {
+	return strings.Replace(s, "\n", "\r\n", -1)
+}
+
 type getHeadersTest struct {
+
+}
+
+var getHeadersTests = []getHeadersTest{
 
 }
 
@@ -13,11 +23,39 @@ func TestGetHeaders(t *testing.T) {
 }
 
 type splitHeadersTest struct {
+	orig, key, val string
+}
 
+var splitHeadersTests = []splitHeadersTest{
+	{
+		`a: b`,
+		`a`, `b`,
+	},
+	{
+		`A1: cD`,
+		`A1`, `cD`,
+	},
+	{
+		crlf(`ab: cd
+ ef`),
+		`ab`, `cd ef`,
+	},
+	{
+		crlf(`ab: cd
+	ef
+	dh`),
+		`ab`, `cd	ef	dh`,
+	},
 }
 
 func TestSplitHeader(t *testing.T) {
-
+	for i, ht := range splitHeadersTests {
+		k, v := splitHeader(ht.orig)
+		if k != ht.key || v != ht.val {
+			t.Errorf(`%d. splitHeader gave ("%s", "%s"), wanted ("%s", "%s")`,
+				i, k, v, ht.key, ht.val)
+		}
+	}
 }
 
 type hdr struct {
