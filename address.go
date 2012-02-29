48325977a528167746a7fff2c34f5ce5959cef68
Scott Lawrence <bytbox@gmail.com>
2012-02-28 20:07:40 -0500

    Distinguishing between groups and mailboxes

--- address.go
+++ address.go
@@ -12,6 +12,19 @@ type MailboxAddr struct {
 type GroupAddr struct {
 }
 
-func ParseAddress(str string) (Address, error) {
+func ParseAddress(bs []byte) (Address, error) {
+	toks, err := tokenize(bs)
+	if err != nil {
+		return nil, err
+	}
+
+	// If this is a group, it must end in a ";" token.
+	ltok := toks[len(toks)-1]
+	if len(ltok) == 1 && ltok[0] == ';' {
+
+	} else {
+
+	}
+
 	return nil, nil
 }
